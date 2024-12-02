ec2dfbc7b6bd0eec21047cc596f604d48e854e32
imagesaller <imagesaller30@gmail.com>
2024-12-02 17:37:33 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  let username = document.getElementById("usernameInput").value;
-  if (username) {
-    window.location.href = "homepage.html";
-  }
-});
